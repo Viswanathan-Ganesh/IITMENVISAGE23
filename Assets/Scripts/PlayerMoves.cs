923b8a07eadfed9ead900f6180aeb450778aca44
Viswanathan-Ganesh <152311052+Viswanathan-Ganesh@users.noreply.github.com>
2023-11-29 22:59:45 +0530

    Project Files

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -7,36 +7,42 @@ using UnityEngine;
 public class PlayerMoves : MonoBehaviour
 {
     public GameObject hitChecker;
-    public float range;
+    public float critRange;
+    public float normalRange;
+    public float normalDamage;
+    public float critDamage;
     public Vector2 dir;
     public Vector3 offset;
-    public Vector2 knockback;
-    public float damage;
+    public Vector2 critBack;
+    public Vector2 knockBack;
+
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 rayStart = transform.position + offset; // creating a raycast
-            RaycastHit2D hit = Physics2D.Raycast(rayStart, dir);
+            Vector2 rayStart = transform.position + offset; // creating start point for the raycast
+            RaycastHit2D hit = Physics2D.Raycast(rayStart, dir); // making a raycast 
             Debug.DrawRay(rayStart, dir);
-            float dis = Mathf.Abs(hit.point.x - rayStart.x);
+            
+            if (hit.collider != null) // checking if the collider hits nothing
+            {
+                if (hit.collider.tag == "Enemy")
+                {
+                    float dis = Mathf.Abs(hit.point.x - rayStart.x); // finding distance at which it hit an object
+                    if (dis <= critRange)
+                    {
+                        hit.collider.GetComponent<EnemyHealth>().SetHealth(critDamage); // gets the componenet EnemyHealth from the collider it hit and calls the function SetHealth
+                        hit.collider.attachedRigidbody.AddForce(critBack * Time.deltaTime, ForceMode2D.Impulse); // adding critBack
+                    }
+                    else if (dis < normalRange && dis > critRange) { }
+                    {
+                        hit.collider.GetComponent<EnemyHealth>().SetHealth(normalDamage); // gets the componenet EnemyHealth from the collider it hit and calls the function SetHealth
+                        hit.collider.attachedRigidbody.AddForce(knockBack * Time.deltaTime, ForceMode2D.Impulse); // adding knockback
+                    }
+                }
+            }
 
-            if (hit.collider.tag == "Enemy")
-            {
-                if (dis < range) 
-                {
-                    hit.collider.GetComponent<EnemyHealth>().SetHealth(damage);
-                }
-                else
-                {
-                    hit.collider.attachedRigidbody.AddForce(knockback * Time.deltaTime, ForceMode2D.Impulse);
-                }
-            }
-            else
-            {
-                Debug.Log("Noone");
-            }
         }
         
     }
