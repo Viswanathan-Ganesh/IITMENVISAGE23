e7506ea81292f0b18f28bb378f9e0c2fa571aebb
Viswanathan-Ganesh <152311052+Viswanathan-Ganesh@users.noreply.github.com>
2024-01-03 14:53:19 +0530

    Project FIles

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 
+
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
     public int maxHealth;
+    public GameObject healthBar;
+    public float healthBarSize;
 
+    void Update()
+    {
+        healthBar.transform.localScale = new Vector3(health * healthBarSize / maxHealth, healthBar.transform.localScale.y, 0f);
+    }
     public void Damage(int damage)
     {
         health -= damage;
